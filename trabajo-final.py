e4e34afbfb6b849a7b5dc5657d54a59972e78c3b
Tinchoutn <93454987+Tinchoutn@users.noreply.github.com>
2021-10-31 18:32:05 -0300

    Update trabajo-final.py

--- trabajo-final.py
+++ trabajo-final.py
@@ -1,6 +1,1 @@
-print("Hola mundo")
-print(2)
-print("Hola a Todos")
-# hola a todos
-print(12112)
-print("dejen de joder y entren por la pagina no hagamos tanto lio por favor se los pido")
+print("hola mundo")
