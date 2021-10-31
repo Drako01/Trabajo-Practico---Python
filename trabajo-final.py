9f083a769ea4f0ca7aaffb0376273ef39ed8cf36
juanlabajian <93482110+juanlabajian@users.noreply.github.com>
2021-10-31 18:35:48 -0300

    Update trabajo-final.py

--- trabajo-final.py
+++ trabajo-final.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+print("Hola mundo")
+print(2)
+print("Hola a Todos")
+# hola a todos
+print(12112)
+print("dejen de joder y entren por la pagina no hagamos tanto lio por favor se los pido")
+print("HOla juan carlo")
+
+
+
+from tkinter import *
+verduleria=["manzana","banana","naranja","tomate","papa","batata","lechuga"]
+print(verduleria[2])
+Label(text=(verduleria))
+mainloop()
