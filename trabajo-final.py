66538f46a5dfd64b2adccf8d2656d557bb70a687
Tinchoutn <93454987+Tinchoutn@users.noreply.github.com>
2021-10-31 20:03:38 -0300

    Update trabajo-final.py

--- trabajo-final.py
+++ trabajo-final.py
@@ -1,33 +1,76 @@
-print("Hola mundo")
-print(2)
-print("Hola a Todos")
-# hola a todos
-print(12112)
-print("dejen de joder y entren por la pagina no hagamos tanto lio por favor se los pido")
-print("HOla juan carlo")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 15:47:56 2021
+todo tkinter explicado paso a paso lo que voy entendiendo
+@author: maromans
+"""
 
 
-#lista
-from tkinter import*
-ventana = Tk()
-ventana.title("2.Ejercicio")
-ventana.geometry("400x50")
+#IMPORTO TKINTER QUE VIENE POR DEFECTO
+import tkinter
 
-lista1 = ["pera" ,"manzana"]
-lista2 = [ "Al mediodia comi una " , " y en la facultad una " ]
-ventana1 = (Label(text= lista2[0] + lista1[0] + lista2[1] + lista1[1])).pack()
 
-mainloop()
 
-#holalaaaaaaa
+#DEFINICION DE FUNCIONES
+"""las funciones se colocan siempre despues del llamado de los modulos por lo que estuve viendo"""
+def prueba():
+    print("esta es la funcion prueba")
 
-from tkinter import *
-ventana = Tk()
-ventana.title("2.Ejercicio")
-ventana.geometry("400x50")
 
-datos={"identificador":"val1","nombre":"juan","apellido":"labajian","telefono":"123456"}
 
-ventana1=(Label(text="los elementos del diccionario son: "+(len(datos))+" y las claves son: "+(datos.keys))).pack()
+"""PROGRAMA PROPIAMENTE DICHO"""
 
-mainloop()
+
+
+
+#ARRANCO DEFINIENDO LAS CLASES DE OBJETOS QUE VAMOS A USAR
+#DEFINO UNA CLASE(capaz que es objeto) DENTRO DE TKINTER
+
+
+#LO MAS GENERAL ES DECIR DONDE VAN ESTAR LAS COSAS ES VENTANA
+
+ventana = tkinter.Tk()# clase dentro del modulo tkinter y le aplico el metodo tk creo
+
+#ALGUNAS COSAS MANEJABLES EN LA VENTANA
+
+ventana.geometry("400x500")#tamaño inicial de ventana
+ventana.resizable(width=False,height=False)#habilito que solo se pueda cambiar en un sentido la ventana
+
+
+"""DEFINIMOS UN ELEMENTO QUE VAN A ESTAR DENTRO DE LA VENTANA QUE PODRIA SER LA ETIQUETA, BOTON CAJA DE TEXTO ETC ETC"""
+
+
+#ETIQUETA
+etiqueta = tkinter.Label(ventana, text ="esto es una prueba", bg = "red" )# PARA QUE LA ETIQUETA SEA ETIQUETA EL METODO ES LABEL
+
+"""a la etiqueta le paso un argumento texto para ponerlo en pantalla y defino el color de fondo en rojo"""
+
+
+#BOTON
+boton1 = tkinter.Button (ventana, text = "aprete aca",  bg = "red" , command =prueba)
+
+
+
+
+#CAJA DE TEXTO
+
+
+
+#METODO DE COLOCACION POR GRILLA
+
+
+
+
+
+#POSISIONADO LAS COSAS EN LA PANTALLA
+""" este que sigue es un metodo de posicionamiento PACK hay otros GRID """
+boton1.pack()
+etiqueta.pack()
+"""este metodo posiciona la etiqueta en pantalla centro arriba acepta otros parametros lo que esta entre los parentesis, va entre los parentesis
+(side=tkinter,RIGHT) por ejemplo lo pone a la derecha etc /BOTH/LEFT/UP
+(fill = tkinter.X) estira en eje x la etiqueta /X/Y  (ojo en el caso de usarlo al y se debe agregar , expand = true)
+"""
+"""DEFINIENDO BOTONES"""
+ventana.mainloop()
+# esto mantiene la ventana corriendo y registra todo lo que le pasa
