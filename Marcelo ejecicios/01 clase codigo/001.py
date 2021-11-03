2e66995f54d551149de9260d88bb10cf2208ba51
maromans <40143234+maromans@users.noreply.github.com>
2021-11-02 23:08:14 -0300

    Delete Marcelo ejecicios directory

--- Marcelo ejecicios/01 clase codigo/001.py
+++
@@ -1,5 +0,0 @@
-print ("hola mundo")
-
-from tkinter import * 
-Label(text='Hola esto es').pack() 
-mainloop() 
--- Marcelo ejecicios/01 clase codigo/prue.py
+++
@@ -1,3 +0,0 @@
-print "me tienen los huevos llenos el github"
-# A VER SI AHORA ANDA. ESTA GARCHA
-# PARECE QUE ANDA ESTA GAR HA
