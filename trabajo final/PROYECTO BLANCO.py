f8c3c7c912fe6e1d3f4064f1d1b9b22cc9babbd4
maromans <40143234+maromans@users.noreply.github.com>
2021-11-04 09:06:52 -0300

    P0000

---
+++ trabajo final/PROYECTO BLANCO.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CRIATURAS, ESTE ES UN BOCETO MIO PROPIO QUE COMPARTO
+CON USTEDES QUE SIRVE PARA SABER DONDE PONER LAS COSAS
+Y LO SEGUI INTENTANDO ORDENARME HACE TIEMPO 
+LO USABA EN MICROPYTHON CON DISTINTAS PLACAS
+GUICreated on Sun Jun 27 18:31:02 2021
+
+@author: maromans
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 17:57:16 2021
+
+@author: maromans IMPORTA PANDAS, QUE NOS PERMITE ACCEDER A LOS DATOS DE UN XLS
+"""
+
+"""
+*********************************************************
+//
+//                 DESCRIPCION DEL PROGRAMA                   //
+*********************************************************
+"""
+
+
+"""
+**********************************************************
+//INICIALIZACION DE VARIABLES Y CONSTANTES                    
+**********************************************************
+"""
+
+"""
+*********************************************************
+//
+//             DECLARACION DE LIBRERIAS INCLUIDAS             //
+//**********************************************************
+"""
+
+
+
+"""
+**********************************************************//
+//            DECLARACION DE FUNCIONES NUEVAS
+IMPORTACION DE LIBRERIAS Y DE DATOS DE FUERA DEL PROGRAMA SIEMPRE DESDE EL DIRECTORIO DEL PROYECTO
+//**********************************************************
+"""
+
+
+"""
+**********************************************************
+// TIPO DE PLACA ESP8266 O ESP32 O ARM 
+//        DESCRIPCION DE CONECTORIZACION EN PLACA                    //
+//               Y LOS SENSORES SERVOS ETC                    //
+//**********************************************************
+"""
+
+
+"""
+**********************************************************
+CONEXIONADO DE ENTRADAS Y SALIDAS EN MICROPYTHON
+**********************************************************
+"""
+
+
+
+"""
+**********************************************************
+PINES CONECTORES PIPES O SALIDAS
+**********************************************************
+"""
+
+
+
+"""
+**********************************************************
+ SUBRRUTINAS O PROCESOS PENDIENTES
+**********************************************************
+//DECLARACION DE SUBRUTINAS//
+//**********************************************************
+"""
+
+
+
+"""
+**********************************************************
+//INFORMACION SOBRE PINES INPUT OUPUT                         //
+//**********************************************************
+"""
+
+"""
+**********************************************************
+//PROGRAMA PROPIAMENTE DICHO EL MAINNNN                         //
+//**********************************************************
+"""
