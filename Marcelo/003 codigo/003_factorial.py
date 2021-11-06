7903c01af8e35fe6a13c1d58b78ed30ae558dada
maromans <40143234+maromans@users.noreply.github.com>
2021-11-06 17:59:53 -0300

    0000

--- Marcelo/003 codigo/003_factorial.py
+++ Marcelo/003 codigo/003_factorial.py
@@ -3,8 +3,10 @@ de un número, intente utilizar funciones o un
 bucle for/in. """
 print (" VAMOS A HACER UN FACTORIAL BUCLE FOR")
 aFactor = int(input("ingrese un numero positivo y entero "))
-a=0
-for x in range(aFactor,0,-1):
-    a=a+x
-print(f" el Factorial de {aFactor} es {a}")
-
+if aFactor >0:
+    a=1
+    for x in range(aFactor,0,-1):
+        a=a*x
+    print(f" el Factorial de {aFactor} es {a}")
+else:
+    print(" el numero ingresado es incorrecto ")
