c91791c262bce402829ac9eaa998c0004c48ac26
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2021-11-09 19:17:39 -0300

    09112021

--- trabajo final/agenda.py
+++ trabajo final/agenda.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 # Le asignamos valores para las dimensiones de la ventana
 master = Tk()
-master.geometry("400x200")
+master.geometry("400x250")
 master.title("Trabajo Final")
 master.resizable(False, False)
 
@@ -12,7 +12,7 @@ encabezado = Label(master, text="Ingrese sus datos",
                    background="#E0FFDA", foreground="black", width=60)
 encabezado.grid(row=0, column=0, columnspan=2)
 # En esta seccion definimos los nombre de variables globales
-agenda={}
+agenda=[]
 
 # En esta seccion estan los Label donde figura el Nombre de cada Campo
 nombre = Label(master, text="Nombre(s)").grid(row=2, column=0, sticky=W)
@@ -39,6 +39,8 @@ entrada_email.grid(row=6, column=1)
 entrada_dni = Entry(master, width=30)
 entrada_dni.grid(row=7, column=1)
 
+lista = []
+
 # Definimos la Funcion callback para Agendar al Contacto
 def callback():
     print("Nuevo Contacto en la Agenda")
@@ -46,17 +48,29 @@ def callback():
           entrada_apellido.get(), '\n', "La Direccion es: ", entrada_direccion.get(),  '\n', "De la Localidad de: ", 
           entrada_localidad.get(), '\n', "El Telefono es: ", entrada_telefono.get(), '\n', "El Correo Electronico es: ", 
           entrada_email.get(), '\n', "El DNI es: ", entrada_dni.get())
-    agenda.Append(entrada_apellido.get,
-              entrada_nombre.get,
-              entrada_direccion.get,
-              entrada_localidad.get,
-              entrada_telefono.get,
-              entrada_email.get,
-              entrada_dni.get)
+
+
+def guardar():
+    
+    lista.append(apellido)
+    lista.append(nombre)
+    lista.append(direccion)
+    lista.append(localidad)
+    lista.append(telefono)
+    lista.append(email)
+    lista.append(dni)
+    print(lista)
+ 
+    
+    
+
+    
             
 
 alta = Button(master, text="Agendar", command=callback, padx=10)
 alta.grid(row=10, column=1)
+alta = Button(master, text="Guardar", command=guardar, padx=10)
+alta.grid(row=12, column=1)
 
 master.mainloop()
 # fin del Programa
