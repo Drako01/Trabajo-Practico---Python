fcd2cf4d3a8b9e367271f171ab3fc86e9305f490
oscar <oscarlely>
2021-11-12 12:13:04 -0300

    cambios del 12/11

--- agenda.py
+++ agenda.py
@@ -6,20 +6,21 @@ lista = set()
 
 # Le asignamos valores para las dimensiones de la ventana
 master = Tk()
-master.geometry("400x250")
+master.geometry("450x250")
 master.title("Trabajo Final")
 master.resizable(False, False)
 
 # Aca vamos a encontrarnos con el Encabezado de la Agenda
-encabezado = Label(master, text="Ingrese sus datos",
-                   background="#E0FFDA", foreground="black", width=60)
+encabezado = Label(
+    master, text="Ingrese sus datos", background="#E0FFDA", foreground="black", width=60
+)
 encabezado.grid(row=0, column=0, columnspan=2)
 # En esta seccion definimos los nombre de variables globales
-agenda={}
+agenda = {}
 
 # En esta seccion estan los Label donde figura el Nombre de cada Campo
-nombre = Label(master, text="Nombre(s)").grid(row=2, column=0, sticky=W)
-apellido = Label(master, text="Apellido").grid(row=1, column=0, sticky=W)
+nombre = Label(master, text="Nombre(s)").grid(row=1, column=0, sticky=W)
+apellido = Label(master, text="Apellido").grid(row=2, column=0, sticky=W)
 direccion = Label(master, text="Dirección").grid(row=3, column=0, sticky=W)
 localidad = Label(master, text="Localidad").grid(row=4, column=0, sticky=W)
 telefono = Label(master, text="Telefono").grid(row=5, column=0, sticky=W)
@@ -45,9 +46,28 @@ entrada_dni.grid(row=7, column=1)
 # Definimos la Funcion callback para Agendar al Contacto
 def callback():
     print("Nuevo Contacto en la Agenda")
-    print(" El Apellido es: ", entrada_apellido.get(),'\n',"El Nombre es: ", entrada_nombre.get(), '\n',   "La Direccion es: ", entrada_direccion.get(),  '\n', "De la Localidad de: ", 
-          entrada_localidad.get(), '\n', "El Telefono es: ", entrada_telefono.get(), '\n', "El Correo Electronico es: ", 
-          entrada_email.get(), '\n', "El DNI es: ", entrada_dni.get())
+    print(
+        " El Apellido es: ",
+        entrada_apellido.get(),
+        "\n",
+        "El Nombre es: ",
+        entrada_nombre.get(),
+        "\n",
+        "La Direccion es: ",
+        entrada_direccion.get(),
+        "\n",
+        "De la Localidad de: ",
+        entrada_localidad.get(),
+        "\n",
+        "El Telefono es: ",
+        entrada_telefono.get(),
+        "\n",
+        "El Correo Electronico es: ",
+        entrada_email.get(),
+        "\n",
+        "El DNI es: ",
+        entrada_dni.get(),
+    )
 
 
 # Definimos los Botones de llamada
