43c82d9fdaca9e43512b0ff3b5f6f1455b9e05cd
maromans <40143234+maromans@users.noreply.github.com>
2021-11-14 16:30:03 -0300

    ver el final

--- trabajo final/descripcionagenda.py
+++ trabajo final/descripcionagenda.py
@@ -1,9 +1,40 @@
 """ QUE CORNNO ARMAMOS DE TODO ESTO
+ENTREGA TRABAJO DIPLOMATURA PYTHON NIVEL INICIAL
+NOS QUEDAMOS CORTOS SI SOLO HACEMOS LA PARTE DE CONTACTOS... RECALCULANDO.... CORTOS
 
-agenda
+DESARROLLO DE UNA AGENDA:
 
-deficinicion de agenda:
+
+
+Deficinicion de agenda: // OJO SIEMPRE AGENDA HABLA DE CALENDARIO ADEMAS DE CONTACTOS
+1.
+Libro o cuaderno donde se apuntan, para no olvidarlas, las cosas que se han de hacer en determinadas fechas; generalmente sus hojas llevan impresos los días del año ordenados por meses y por semanas con un espacio para escribir.
+2.
+Programa de actividades o de trabajo que pretende realizar una persona en un período determinado de tiempo.
+"tener una agenda muy apretada"
+
+Qué es Agenda:
+Como agenda se denomina el programa que contiene, ordenadamente, un conjunto de temas, tareas o actividades para su realización en un periodo de tiempo determinado. Como tal, la palabra proviene del latín agenda, que significa ‘cosas que se han de hacer’.
+
+OTRA DEFICINICION
+Como agenda electrónica o digital se denomina el dispositivo electrónico de bolsillo 
+que funciona como una agenda personal. Como tal, posee múltiples funciones orientadas 
+a la gestión del tiempo, y tiene la capacidad de almacenar todo tipo datos, 
+así como de organizar tareas y actividades. 
+Combina programación de actividades, 
+lista de contactos, 
+bloc de notas, alarmas y recordatorios. 
+Hoy día, no obstante, han sido desplazadas por aplicaciones que
+se integran en el sistema operativo de computadoras personales, tablets y teléfonos inteligentes.
+
+
+
+
+
+
 llave key logearse:
+
+OBJETIVO DAR DE ALTA UN USUARIO Y UNA CLAVE PARA LA AGENDA
 
 objetivo del proyecto:
 
@@ -66,4 +97,92 @@ FRUTILLAS DEL POSTRE:
 
 
 """
-print("saludos maromans")
+print("cagamos una agenda es mas jodida que un libro de contactos")
+
+
+"""
+TRABAJO FINAL DIPLOMATURA PYTHON UTN  elearning NIVEL INICIAL
+------------------------------------------------------------------------------------------------------------
+
+OBJETIVO DEL TRABAJO “Desarrollo de un aplicativo en python que utiliza  o no interfaz gráfica ( tkinter) cuya funcionalidad sea la de una agenda de datos que se vincule con tablas externas al programa, creadas por este, aplicando todo lo conocido y visto en el curso inicial de la diplomatura
+ 
+------------------------------------------------------------------------------------------------------------
+INTEGRANTES DEL TRABAJO
+Nosotros
+
+------------------------------------------------------------------------------------------------------------ 
+DEFINICIÓN DE AGENDA:
+ 
+Etimologia: la palabra agenda proviene de latin “agendum” (lo que se debe hacer), es su plural neutro (“cosas para hacer”) es la forma neutra del participio de futuro pasivo, es decir lo que esta en una agenda es algo a realizarse desde su etimología. Implica futuro de realización.
+ 
+------------------------------------------------------------------------------------------------------------
+ 
+DESARROLLO:
+LA AGENDA DESARROLLADA CONSTA DE VARIOS MÓDULOS INTERRELACIONADOS
+
+1. 	CONTACTOS
+2. 	TAREAS
+3. 	RECORDATORIOS
+4. 	CALENDARIOS (CITAS DE CALENDARIO)
+5. 	NOTAS CON ALARMA
+
+------------------------------------------------------------------------------------------------------------
+ 
+1) SENTANDO BASES DEFINIENDO ESTRUCTURAS TABLAS
+ 
+2) INTERRELACIONADO  LAS ESTRUCTURAS
+
+Con el fin de contestar estas dos premisas desarrollaremos cada uno de los módulos separadamente (forma de adquisición de datos), y luego accederemos a ellas desde una pantalla principal de nuestra aplicación. Realizado esto tendremos definidos los campos y las validaciones necesarias
+ ------------------------------------------------------------------------------------------------------------
+
+3) INGRESO DE DATOS (que definirán tablas)
+4) VALIDACION DE LOS MISMOS ANTES DE INTEGRAR LAS TABLAS
+
+------------------------------------------------------------------------------------------------------------
+CONTACTOS
+1. 	INGRESO DE DATOS: NOMBRE, APELLIDO, FECHA NAC, GENERO, DIRECCION, LOCALIDAD, TELEFONO, EMAIL
+2. 	VERIFICACIÓN DE CAMPOS EN BLANCO
+3. 	VERIFICACIÓN DE DUPLICACIÓN DE DATOS
+4. 	VERIFICACIÓN TELEFONO SUPERIOR A 10 CIFRAS
+5. 	VERIFICACIÓN DE EMAIL CONTENIENDO @
+------------------------------------------------------------------------------------------------------------
+        	           
+TAREAS
+1. 	VERIFICACIÓN DE INGRESO DE TAREAS
+2. 	VERIFICACIÓN DE DATOS NO EN BLANCO
+3. 	VERIFICACIÓN DE CAMPO DE FECHA MAYOR QUE CAMPO HOY
+4. 	PRIORIDAD DE LA TAREA CREADA EN ESCALA DE 1 A 3
+5. 	PORCENTAJE DE TAREA COMPLETADA
+ ------------------------------------------------------------------------------------------------------------
+RECORDATORIO
+1. 	INGRESO DE DATOS
+2. 	VERIFICACIÓN DE DATOS NO EN BLANCO
+3. 	VERIFICACIÓN DE INCONSISTENCIAS
+4. 	VERIFICACIÓN DE FECHAS MAYOR A LA FECHA ACTUAL
+5. 	VERIFICACIÓN DE RECORDATORIO NO DUPLICADO
+6. 	COLOCACIÓN O NO DE ALARMA
+ ------------------------------------------------------------------------------------------------------------
+NOTAS
+1. 	INGRESO DE DATOS
+2. 	VERIFICACIÓN DE DATOS NO EN BLANCO
+3. 	VERIFICACIÓN DE INCONSISTENCIAS
+4. 	VERIFICACIÓN DE FECHAS MAYOR A LA FECHA ACTUAL
+5. 	VERIFICACIÓN DE RECORDATORIO NO DUPLICADO
+6. 	COLOCACIÓN O NO DE ALARMA
+
+------------------------------------------------------------------------------------------------------------
+
+
+5) BÚSQUEDA DE DATOS POR:
+
+CONTACTOS (APELLIDO, LOCALIDADES SEGMENTACIÓN (RANGO ETARIOS)
+TAREAS (NOMBRE TAREA, UBICACION, FECHA TERMINACIÓN % DE REALIZACIÓN)
+RECORDATORIOS (ASUNTO, FECHAS, PALABRAS CLAVE)
+CALENDARIO (FECHAS)
+NOTAS (ASUNTO, FECHA, PALABRAS CLAVE)
+------------------------------------------------------------------------------------------------------------
+           
+
+
+
+"""
