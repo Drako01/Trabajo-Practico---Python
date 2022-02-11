71b7dd90e2e14974cf93797ae9c7dc0e56126973
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2022-02-11 18:47:09 -0300

    11022022

---
+++ Trabajo_Final_Intermedio/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'Agenda de Contacto',
+    version = '1.0.0',
+    py_modules = ['agenda'],
+    author = 'Los Pythonisos',
+    author_email = 'oscarlely@gmail.com',
+    url = 'armotusitio.com',
+    description = 'Entrega Intermedia',
+    )
